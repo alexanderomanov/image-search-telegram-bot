d8f269b7041b7ec86a6970312124e95ea69ca709
alexanderomanov <shura.vasilkov.0@list.ru>
2022-05-09 01:00:52 +0300

    Написал Телеграмм-бота для поиска по картинкам

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const {Telegraf} = require('telegraf')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Ищу картинки в Гугле и Яндексе. Отправь мне фото, чтобы начать.'))
+bot.help((ctx) => ctx.reply('Ищу картинки в Гугле и Яндексе. Отправь мне фото, чтобы начать.'))
+bot.on('photo', async (ctx) => {
+    const fileId = ctx.message.photo[ctx.message.photo.length - 1].file_id
+    const imageUrl = await ctx.telegram.getFileLink(fileId)
+    const href = imageUrl.href
+    const yaUrl = 'https://yandex.by/images/search?rpt=imageview&url=' + href
+    const goUrl = 'https://www.google.com/searchbyimage?image_url=' + href
+    ctx.reply('Yandex: \n' + yaUrl, { disable_web_page_preview: true })
+    ctx.reply('Google: \n' + goUrl, { disable_web_page_preview: true })
+})
+
+bot.launch()
+
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
